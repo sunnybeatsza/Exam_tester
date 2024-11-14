e9bc5ec14bc7afbd47cd301ad05aa1a79b4e5345
momakgejhb024 <momakgejhb024@student.wethinkcode.co.za>
2024-11-14 15:44:20 +0200

    set questions and read file

--- exam_tester.py
+++ exam_tester.py
@@ -1,11 +1,27 @@
-def read_file():
-    pass
+import random
 
-def shuffle_questions():
-    pass
+
+def read_file(text):
+    with open(text) as questions:
+        list_of_questions = questions.readlines()
+
+        return list_of_questions
+
+def set_questions(questions):
+    return random.sample(questions, k=10)
 
 def check_answers():
     pass
 
 def produce_marks():
     pass
+
+
+def exam_tester(text):
+    questions = read_file(text)
+    questions_list = set_questions(questions)
+    for question in questions_list:
+        print(question)
+
+    
+exam_tester("Questions for CompTia Security.txt")
